7b7d4f3a82118857ab567c8512ec73416115b6ea
Thiago <andradethiago1609@gmail.com>
2023-11-03 20:44:31 -0300

    .

--- src/methods.h
+++ src/methods.h
@@ -21,8 +21,8 @@ void init(char game[3][3])
     {
         if (l == 0)
         {
-            printf("\n\n");
-            printf("\t\t\t\t\t    1    2    3  \n\n");
+            printf("\n\t\t\t\t\t      Colunas\n\n");
+            printf("\t\t\t\t\t    1    2    3   \n\n");
         }
 
         for (int c = 0; c < 3; c++)
@@ -91,81 +91,6 @@ bool checkWin(char player, char game[3][3])
     return false;
 }
 
-void showGame(char game[3][3], int player, int line, int column, int moves, int win, int play_again)
-{
-    do
-    {
-        // monta a matriz
-        for (int x = 0; x < 3; x++)
-        {
-            for (int y = 0; y < 3; y++)
-            {
-                game[x][y] = ' ';
-            }
-        }
-        init(game);
-        do
-        {
-            clearScreen();
-            init(game);
-            printf("\n\nJogador %d digite a linha e coluna que deseja jogar: ", player);
-            scanf("%d%d", &line, &column);
 
-            fflush(stdin);
-            printf(" %d %d ", line, column);
-            line--;
-            column--;
-
-            if ((checkMove(line, column)) && (game[line][column] == ' '))
-            {
-
-                clearScreen();
-                if (player == 1)
-                {
-                    game[line][column] = 'X';
-                    player++;
-                }
-                else
-                {
-                    game[line][column] = 'O';
-                    player = 1;
-                }
-                init(game);
-                moves++;
-                if (checkWin('X', game))
-                {
-
-                    printf("\n\n\n\t\t\t\t  O player 1 venceu o game!!\n\n\n");
-                    Sleep(1500);
-                    win = 1;
-                    break;
-                }
-                else if (checkWin('O', game))
-                {
-
-                    printf("\n\n\n\t\t\t\t  O player 2 venceu o game!!\n\n\n");
-                    Sleep(1500);
-                    win = 1;
-                    break;
-                }
-            }
-
-            if (moves == 9)
-            {
-                printf("\n \t\t\t\t  EMPATE!\n\n");
-            }
-        } while (moves < 9 && win == 0);
-
-        printf("\t\tDigite 1 caso queira jogar novamente ou qualquer tecla para parar de jogar.\n");
-        scanf("%d", &play_again);
-        fflush(stdin);
-
-        if (play_again != 1)
-        {
-            exit(1);
-        }
-
-    } while (play_again == 1);
-}
 
 #endif
--- src/tictactoe.c
+++ src/tictactoe.c
@@ -7,7 +7,81 @@ char game[3][3];
 int main()
 {
     init(game);
-    showGame(game, player, line, column, moves, win, play_again);
+    do
+    {
+        // monta a matriz
+        for (int x = 0; x < 3; x++)
+        {
+            for (int y = 0; y < 3; y++)
+            {
+                game[x][y] = ' ';
+            }
+        }
+        init(game);
+        do
+        {
+            clearScreen();
+            init(game);
+            printf("\n\nJogador %d digite a linha que deseja jogar: ", player);
+            scanf("%d", &line);
+            printf("Jogador %d digite a coluna que deseja jogar: ", player);
+            scanf("%d", &column);
+
+            fflush(stdin);
+            printf(" %d %d ", line, column);
+            line--;
+            column--;
+
+            if ((checkMove(line, column)) && (game[line][column] == ' '))
+            {
+
+                clearScreen();
+                if (player == 1)
+                {
+                    game[line][column] = 'X';
+                    player++;
+                }
+                else
+                {
+                    game[line][column] = 'O';
+                    player = 1;
+                }
+                init(game);
+                moves++;
+                if (checkWin('X', game))
+                {
+
+                    printf("\n\n\n\t\t\t\t  O player 1 venceu o game!!\n\n\n");
+                    Sleep(1500);
+                    win = 1;
+                    break;
+                }
+                else if (checkWin('O', game))
+                {
+
+                    printf("\n\n\n\t\t\t\t  O player 2 venceu o game!!\n\n\n");
+                    Sleep(1500);
+                    win = 1;
+                    break;
+                }
+            }
+
+            if (moves == 9)
+            {
+                printf("\n \t\t\t\t  EMPATE!\n\n");
+            }
+        } while (moves < 9 && win == 0);
+
+        printf("\t\tDigite 1 caso queira jogar novamente ou qualquer tecla para parar de jogar.\n");
+        scanf("%d", &play_again);
+        fflush(stdin);
+
+        if (play_again != 1)
+        {
+            exit(1);
+        }
+
+    } while (play_again == 1);
 
     return 0;
 }
